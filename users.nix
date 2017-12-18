55416f7d72d8bf32b7ad7236c65681cbea8fa2de
Patrick Lambein <patric@lambein.name>
2017-12-18 11:03:15 +0100

    Set global variable EDITOR to nvim

--- users.nix
+++ users.nix
@@ -5,6 +5,11 @@
     ./users/p.nix
   ];
 
-  programs.zsh.enable    = true;
-  users.defaultUserShell = pkgs.zsh;
+  programs.zsh.enable               = true;
+  users.defaultUserShell            = pkgs.zsh;
+  security.sudo.wheelNeedsPassword  = false;
+
+  environment.sessionVariables = {
+    EDITOR = "nvim";
+  };
 }
